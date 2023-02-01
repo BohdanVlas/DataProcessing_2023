4639f72fc735055054ec1d6a7df326ca4de21cbf
BohdanVlas <bodik.vlasenko@gmail.com>
2023-02-01 08:33:43 +0200

    Lab_1 added

---
+++ Lab_1/src/main/java/Servlets/Servlet_1.java
@@ -0,0 +1,15 @@
+package Servlets;
+
+
+
+@WebServlet
+
+public class Servlet_1 extends HttpServlet {
+
+    @Override
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) {
+
+    }
+
+}
